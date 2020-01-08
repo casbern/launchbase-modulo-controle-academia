f6735b9d5fc07db99aba78aceedf529e0e9eccbc
cassia <cascrisbern@gmail.com>
2020-01-08 12:14:25 -0300

    send the data from the form

--- routes.js
+++ routes.js
@@ -13,8 +13,8 @@ routes.get('/instructors/create', function(req, res) {
   return res.render('instructors/create')
 })
 
-routes.post('/instructors', function(req, res) {
-  return res.send("recebido")
+routes.post("/instructors", function(req, res) {
+  return res.send(req.body)
 })
 
 routes.get('/members', function(req, res) {
