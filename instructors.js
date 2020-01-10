7f89c3fcada466c402ed8a996be25265643fb9f9
cassia <cascrisbern@gmail.com>
2020-01-10 15:34:52 -0300

    showing data you collected

--- instructors.js
+++ instructors.js
@@ -1,6 +1,19 @@
 const fs = require('fs')
 const data = require('./data.json')
 
+//show
+exports.show = function(req, res) {
+  const { id } = req.params
+
+  const findInstructor = data.instructors.find(function(instructor) {
+    return id == instructor.id
+  })
+
+  if (!findInstructor) return res.send('Instructor not found!')
+
+  return res.send(findInstructor)
+}
+
 // create
 exports.post = function(req, res) {
 
@@ -11,7 +24,23 @@ exports.post = function(req, res) {
         return res.send('Please, fill all the form.')
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+    let { avatar_url, birth, name, services, gender } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+      id,
+      avatar_url,
+      name,
+      birth,
+      gender,
+      services,
+      created_at,
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
       if(err) return res.send("Write file error")
   
       return res.redirect("/instructors")
