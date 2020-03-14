68c9546a0571cb33c39f2bf4cce57f8cf05dd64d
Cássia <cascrisbern@gmail.com>
2020-03-14 18:59:48 -0300

    -

--- routes.js
+++ routes.js
@@ -18,7 +18,6 @@ routes.delete("/instructors", instructors.delete)
 
 
 routes.get('/members', members.index)
-
 routes.get('/members/create', members.create)
 routes.get('/members/:id', members.show)
 routes.get('/members/:id/edit', members.edit)
